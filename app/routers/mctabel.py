be3c299b8e3692e9d9b2b27411fa2dd30c441dc2
Wouter Hoenderdaal <wouter.hoenderdaal@wur.nl>
2026-08-30 12:21:59 +0200

    speed things up

--- app/routers/mctabel.py
+++ app/routers/mctabel.py
@@ -1,4 +1,5 @@
 import logging
+from time import perf_counter
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -97,6 +98,7 @@ def save_mctabel(
     rows: List[MCTabel],
     current_user: int = Depends(get_current_user),
 ):
+    total_start = perf_counter()
     if not rows:
         return MCTabelSaveResponse(
             received=0,
@@ -105,12 +107,15 @@ def save_mctabel(
             unchanged=0,
         )
 
+    step_start = perf_counter()
     keys = [(row.sheetname, row.id) for row in rows]
     if len(keys) != len(set(keys)):
         raise HTTPException(
             status_code=400,
             detail="The submitted data contains duplicate (sheetname, id) combinations.",
         )
+
+    timing_keys = perf_counter() - step_start
 
     create_temp_sql = """
         CREATE TEMP TABLE tmp_mctabel_save
@@ -123,9 +128,31 @@ def save_mctabel(
         VALUES %s
     """
 
-    lock_output_sql = """
-        LOCK TABLE mc.tabel_output
-        IN SHARE ROW EXCLUSIVE MODE;
+    lock_existing_sql = """
+        SELECT output.id
+        FROM mc.tabel_output AS output
+        INNER JOIN tmp_mctabel_save AS incoming
+            ON incoming.sheetname = output.sheetname
+           AND incoming.id = output.id
+        FOR UPDATE OF output;
+    """
+
+    create_changed_sql = """
+        CREATE TEMP TABLE tmp_mctabel_changed
+        ON COMMIT DROP
+        AS
+        SELECT
+            incoming.sheetname,
+            incoming.id
+        FROM tmp_mctabel_save AS incoming
+        INNER JOIN mc.tabel_output AS output
+            ON incoming.sheetname = output.sheetname
+           AND incoming.id = output.id
+        WHERE ROW(output.team_id, output.groupori, output.use, output.productgroup_id, output.sample_matrix, output.e02_sampmatcode1_en, output.e02_sampmatcode1_nl, output.e02_sampmatcode2_en, output.e02_sampmatcode2_nl, output.e02_sampmatcode3_en, output.e02_sampmatcode3_nl, output.e02_sampmatcode4_en, output.e02_sampmatcode4_nl, output.mtx_id, output.substance_group, output.param_id, output.param_termextendedname, output.paramtext_lims, output.paramtext_abbreviation, output.paramtyp_id, output.anmethodref, output.flex_scope_no, output.qual_quan_method, output.anlytyp_id, output.anlymd_id, output.mdacc_id, output.resinfo, output.resunit_wfsr, output.unit_id, output.exprres_id, output.lod, output.loq, output.ccalpha, output.ccbeta, output.resvaluncert, output.evallowlimit, output.actionlevel, output.lmttyp_id, output.confirmation_sop, output.lu_s_productid, output.detailedcom, output.val_report_name, output.val_report_date, output.matrix_cal_curve, output.measuring_range, output.trueness_j_recovery, output.rsdr, output.rsdwr_rsdrl, output.mutation_date, output.plan_nvwa_year, output.remarks, output.insert_date)
+              IS DISTINCT FROM
+              ROW(incoming.team_id, incoming.groupori, incoming.use, incoming.productgroup_id, incoming.sample_matrix, incoming.e02_sampmatcode1_en, incoming.e02_sampmatcode1_nl, incoming.e02_sampmatcode2_en, incoming.e02_sampmatcode2_nl, incoming.e02_sampmatcode3_en, incoming.e02_sampmatcode3_nl, incoming.e02_sampmatcode4_en, incoming.e02_sampmatcode4_nl, incoming.mtx_id, incoming.substance_group, incoming.param_id, incoming.param_termextendedname, incoming.paramtext_lims, incoming.paramtext_abbreviation, incoming.paramtyp_id, incoming.anmethodref, incoming.flex_scope_no, incoming.qual_quan_method, incoming.anlytyp_id, incoming.anlymd_id, incoming.mdacc_id, incoming.resinfo, incoming.resunit_wfsr, incoming.unit_id, incoming.exprres_id, incoming.lod, incoming.loq, incoming.ccalpha, incoming.ccbeta, incoming.resvaluncert, incoming.evallowlimit, incoming.actionlevel, incoming.lmttyp_id, incoming.confirmation_sop, incoming.lu_s_productid, incoming.detailedcom, incoming.val_report_name, incoming.val_report_date, incoming.matrix_cal_curve, incoming.measuring_range, incoming.trueness_j_recovery, incoming.rsdr, incoming.rsdwr_rsdrl, incoming.mutation_date, incoming.plan_nvwa_year, incoming.remarks, incoming.insert_date);
+
+        CREATE INDEX ON tmp_mctabel_changed (sheetname, id);
     """
 
     archive_sql = """
@@ -135,12 +162,9 @@ def save_mctabel(
         SELECT
             output.id, output.team_id, output.groupori, output.use, output.productgroup_id, output.sample_matrix, output.e02_sampmatcode1_en, output.e02_sampmatcode1_nl, output.e02_sampmatcode2_en, output.e02_sampmatcode2_nl, output.e02_sampmatcode3_en, output.e02_sampmatcode3_nl, output.e02_sampmatcode4_en, output.e02_sampmatcode4_nl, output.mtx_id, output.substance_group, output.param_id, output.param_termextendedname, output.paramtext_lims, output.paramtext_abbreviation, output.paramtyp_id, output.anmethodref, output.flex_scope_no, output.qual_quan_method, output.anlytyp_id, output.anlymd_id, output.mdacc_id, output.resinfo, output.resunit_wfsr, output.unit_id, output.exprres_id, output.lod, output.loq, output.ccalpha, output.ccbeta, output.resvaluncert, output.evallowlimit, output.actionlevel, output.lmttyp_id, output.confirmation_sop, output.lu_s_productid, output.detailedcom, output.val_report_name, output.val_report_date, output.matrix_cal_curve, output.measuring_range, output.trueness_j_recovery, output.rsdr, output.rsdwr_rsdrl, output.mutation_date, output.plan_nvwa_year, output.remarks, output.insert_date, output.sheetname
         FROM mc.tabel_output AS output
-        INNER JOIN tmp_mctabel_save AS incoming
-            ON incoming.sheetname = output.sheetname
-           AND incoming.id = output.id
-        WHERE ROW(output.team_id, output.groupori, output.use, output.productgroup_id, output.sample_matrix, output.e02_sampmatcode1_en, output.e02_sampmatcode1_nl, output.e02_sampmatcode2_en, output.e02_sampmatcode2_nl, output.e02_sampmatcode3_en, output.e02_sampmatcode3_nl, output.e02_sampmatcode4_en, output.e02_sampmatcode4_nl, output.mtx_id, output.substance_group, output.param_id, output.param_termextendedname, output.paramtext_lims, output.paramtext_abbreviation, output.paramtyp_id, output.anmethodref, output.flex_scope_no, output.qual_quan_method, output.anlytyp_id, output.anlymd_id, output.mdacc_id, output.resinfo, output.resunit_wfsr, output.unit_id, output.exprres_id, output.lod, output.loq, output.ccalpha, output.ccbeta, output.resvaluncert, output.evallowlimit, output.actionlevel, output.lmttyp_id, output.confirmation_sop, output.lu_s_productid, output.detailedcom, output.val_report_name, output.val_report_date, output.matrix_cal_curve, output.measuring_range, output.trueness_j_recovery, output.rsdr, output.rsdwr_rsdrl, output.mutation_date, output.plan_nvwa_year, output.remarks, output.insert_date)
-              IS DISTINCT FROM
-              ROW(incoming.team_id, incoming.groupori, incoming.use, incoming.productgroup_id, incoming.sample_matrix, incoming.e02_sampmatcode1_en, incoming.e02_sampmatcode1_nl, incoming.e02_sampmatcode2_en, incoming.e02_sampmatcode2_nl, incoming.e02_sampmatcode3_en, incoming.e02_sampmatcode3_nl, incoming.e02_sampmatcode4_en, incoming.e02_sampmatcode4_nl, incoming.mtx_id, incoming.substance_group, incoming.param_id, incoming.param_termextendedname, incoming.paramtext_lims, incoming.paramtext_abbreviation, incoming.paramtyp_id, incoming.anmethodref, incoming.flex_scope_no, incoming.qual_quan_method, incoming.anlytyp_id, incoming.anlymd_id, incoming.mdacc_id, incoming.resinfo, incoming.resunit_wfsr, incoming.unit_id, incoming.exprres_id, incoming.lod, incoming.loq, incoming.ccalpha, incoming.ccbeta, incoming.resvaluncert, incoming.evallowlimit, incoming.actionlevel, incoming.lmttyp_id, incoming.confirmation_sop, incoming.lu_s_productid, incoming.detailedcom, incoming.val_report_name, incoming.val_report_date, incoming.matrix_cal_curve, incoming.measuring_range, incoming.trueness_j_recovery, incoming.rsdr, incoming.rsdwr_rsdrl, incoming.mutation_date, incoming.plan_nvwa_year, incoming.remarks, incoming.insert_date);
+        INNER JOIN tmp_mctabel_changed AS changed
+            ON changed.sheetname = output.sheetname
+           AND changed.id = output.id;
     """
 
     update_existing_sql = """
@@ -199,11 +223,11 @@ def save_mctabel(
             remarks = incoming.remarks,
             insert_date = incoming.insert_date
         FROM tmp_mctabel_save AS incoming
+        INNER JOIN tmp_mctabel_changed AS changed
+            ON changed.sheetname = incoming.sheetname
+           AND changed.id = incoming.id
         WHERE incoming.sheetname = output.sheetname
-          AND incoming.id = output.id
-          AND ROW(output.team_id, output.groupori, output.use, output.productgroup_id, output.sample_matrix, output.e02_sampmatcode1_en, output.e02_sampmatcode1_nl, output.e02_sampmatcode2_en, output.e02_sampmatcode2_nl, output.e02_sampmatcode3_en, output.e02_sampmatcode3_nl, output.e02_sampmatcode4_en, output.e02_sampmatcode4_nl, output.mtx_id, output.substance_group, output.param_id, output.param_termextendedname, output.paramtext_lims, output.paramtext_abbreviation, output.paramtyp_id, output.anmethodref, output.flex_scope_no, output.qual_quan_method, output.anlytyp_id, output.anlymd_id, output.mdacc_id, output.resinfo, output.resunit_wfsr, output.unit_id, output.exprres_id, output.lod, output.loq, output.ccalpha, output.ccbeta, output.resvaluncert, output.evallowlimit, output.actionlevel, output.lmttyp_id, output.confirmation_sop, output.lu_s_productid, output.detailedcom, output.val_report_name, output.val_report_date, output.matrix_cal_curve, output.measuring_range, output.trueness_j_recovery, output.rsdr, output.rsdwr_rsdrl, output.mutation_date, output.plan_nvwa_year, output.remarks, output.insert_date)
-              IS DISTINCT FROM
-              ROW(incoming.team_id, incoming.groupori, incoming.use, incoming.productgroup_id, incoming.sample_matrix, incoming.e02_sampmatcode1_en, incoming.e02_sampmatcode1_nl, incoming.e02_sampmatcode2_en, incoming.e02_sampmatcode2_nl, incoming.e02_sampmatcode3_en, incoming.e02_sampmatcode3_nl, incoming.e02_sampmatcode4_en, incoming.e02_sampmatcode4_nl, incoming.mtx_id, incoming.substance_group, incoming.param_id, incoming.param_termextendedname, incoming.paramtext_lims, incoming.paramtext_abbreviation, incoming.paramtyp_id, incoming.anmethodref, incoming.flex_scope_no, incoming.qual_quan_method, incoming.anlytyp_id, incoming.anlymd_id, incoming.mdacc_id, incoming.resinfo, incoming.resunit_wfsr, incoming.unit_id, incoming.exprres_id, incoming.lod, incoming.loq, incoming.ccalpha, incoming.ccbeta, incoming.resvaluncert, incoming.evallowlimit, incoming.actionlevel, incoming.lmttyp_id, incoming.confirmation_sop, incoming.lu_s_productid, incoming.detailedcom, incoming.val_report_name, incoming.val_report_date, incoming.matrix_cal_curve, incoming.measuring_range, incoming.trueness_j_recovery, incoming.rsdr, incoming.rsdwr_rsdrl, incoming.mutation_date, incoming.plan_nvwa_year, incoming.remarks, incoming.insert_date);
+          AND incoming.id = output.id;
     """
 
     insert_new_sql = """
@@ -221,35 +245,58 @@ def save_mctabel(
         );
     """
 
+    step_start = perf_counter()
     values = [(row.id, row.team_id, row.groupori, row.use, row.productgroup_id, row.sample_matrix, row.e02_sampmatcode1_en, row.e02_sampmatcode1_nl, row.e02_sampmatcode2_en, row.e02_sampmatcode2_nl, row.e02_sampmatcode3_en, row.e02_sampmatcode3_nl, row.e02_sampmatcode4_en, row.e02_sampmatcode4_nl, row.mtx_id, row.substance_group, row.param_id, row.param_termextendedname, row.paramtext_lims, row.paramtext_abbreviation, row.paramtyp_id, row.anmethodref, row.flex_scope_no, row.qual_quan_method, row.anlytyp_id, row.anlymd_id, row.mdacc_id, row.resinfo, row.resunit_wfsr, row.unit_id, row.exprres_id, row.lod, row.loq, row.ccalpha, row.ccbeta, row.resvaluncert, row.evallowlimit, row.actionlevel, row.lmttyp_id, row.confirmation_sop, row.lu_s_productid, row.detailedcom, row.val_report_name, row.val_report_date, row.matrix_cal_curve, row.measuring_range, row.trueness_j_recovery, row.rsdr, row.rsdwr_rsdrl, row.mutation_date, row.plan_nvwa_year, row.remarks, row.insert_date, row.sheetname) for row in rows]
+    timing_values = perf_counter() - step_start
 
+    step_start = perf_counter()
     with PostgresDatabase(
         DB_NAME,
         DB_USER,
         DB_PASSWORD,
         realdictcursor=False,
     ) as db:
-        try:
-            db.execute(create_temp_sql)
+        timing_connection = perf_counter() - step_start
 
+        try:
+            step_start = perf_counter()
+            db.execute(create_temp_sql)
+            timing_create_temp = perf_counter() - step_start
+
+            step_start = perf_counter()
             execute_values(
                 db.cursor,
                 insert_temp_sql,
                 values,
                 page_size=1000,
             )
+            timing_temp_insert = perf_counter() - step_start
 
-            db.execute(lock_output_sql)
+            step_start = perf_counter()
+            db.execute(lock_existing_sql)
+            timing_lock = perf_counter() - step_start
 
+            step_start = perf_counter()
+            db.execute(create_changed_sql)
+            timing_compare = perf_counter() - step_start
+
+            step_start = perf_counter()
             db.execute(archive_sql)
+            timing_archive = perf_counter() - step_start
 
+            step_start = perf_counter()
             db.execute(update_existing_sql)
             updated_count = db.cursor.rowcount
+            timing_update = perf_counter() - step_start
 
+            step_start = perf_counter()
             db.execute(insert_new_sql)
             inserted_count = db.cursor.rowcount
+            timing_insert = perf_counter() - step_start
 
+            step_start = perf_counter()
             db.commit()
+            timing_commit = perf_counter() - step_start
 
         except Exception as exc:
             db.rollback()
@@ -260,6 +307,27 @@ def save_mctabel(
             ) from exc
 
     unchanged_count = len(rows) - updated_count - inserted_count
+    timing_total = perf_counter() - total_start
+
+    logger.warning(
+        "MC TABEL SAVE TIMING | rows=%d | keys=%.3fs | values=%.3fs | "
+        "connection=%.3fs | create_temp=%.3fs | temp_insert=%.3fs | "
+        "lock=%.3fs | compare=%.3fs | archive=%.3fs | update=%.3fs | "
+        "insert=%.3fs | commit=%.3fs | total=%.3fs",
+        len(rows),
+        timing_keys,
+        timing_values,
+        timing_connection,
+        timing_create_temp,
+        timing_temp_insert,
+        timing_lock,
+        timing_compare,
+        timing_archive,
+        timing_update,
+        timing_insert,
+        timing_commit,
+        timing_total,
+    )
 
     return MCTabelSaveResponse(
         received=len(rows),
